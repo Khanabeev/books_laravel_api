e4abac57bf945ebd31e9a54587447d6d398a948b
Anton Khanabeev <khanabeev@gmail.com>
2020-11-19 20:10:09 +0300

    add authors_list to Book model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -15,8 +15,24 @@ class Book extends Model
         'description'
     ];
 
+    protected $appends = [
+        'authors_list'
+    ];
+
     public function authors()
     {
         return $this->belongsToMany(Author::class, 'author_book');
+    }
+
+    public function getAuthorsListAttribute()
+    {
+        $authorsList = [];
+        $this->authors()->each(function ($author) use ($authorsList) {
+            $familyName = ucfirst(mb_strtolower($author->family_name));
+            $firstNameLatter = mb_strtoupper(substr($author->first_name, 0, 1));
+            $middleNameLatter = mb_strtoupper(substr($author->middle_name, 0, 1));
+            $authorsList[] = $familyName . " " . $firstNameLatter . "." . $middleNameLatter . ".";
+        });
+        return implode(' ,', $authorsList) ?? "";
     }
 }

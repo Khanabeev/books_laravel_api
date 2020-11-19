6dfde970ed2e41081b5ab2a23f0afa887a683c4d
Anton Khanabeev <khanabeev@gmail.com>
2020-11-19 21:51:56 +0300

    changed api route to apiResource

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Api\v1\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('/v1')->group(function() {
-    Route::get('/authors', [AuthorController::class, 'index']);
-    Route::get('/books', [BookController::class, 'index']);
+Route::prefix('/v1')->group(function () {
+    Route::apiResource('authors', AuthorController::class);
+    Route::apiResource('books', BookController::class);
 });
